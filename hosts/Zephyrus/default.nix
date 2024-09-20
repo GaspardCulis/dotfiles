71847837d7ce062d88a8b220d10700d361c7c6be
GaspardCulis <gaspard@gasdev.fr>
2024-09-20 19:44:48 +0200

    feat(Zephyrus): Setup wifi and bluetooth

--- hosts/Zephyrus/default.nix
+++ hosts/Zephyrus/default.nix
@@ -9,13 +9,52 @@
     ./hardware-configuration.nix
   ];
 
+  # Nix
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Set your time zone.
   time.timeZone = "Europe/Paris";
 
   console.keyMap = "fr";
 
-  # Nix
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # Network & Bluetooth
+  networking.wireless.iwd.enable = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Services
+  services.seatd.enable = true;
+  services.blueman.enable = true;
+  services.power-profiles-daemon.enable = true;
+
+  # Programs
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.ncdu
+    pkgs.neofetch
+    pkgs.bottom
+    pkgs.htop
+    pkgs.iwgtk
+  ];
+
+  # NVIDIA
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["nvidia-x11" "nvidia-settings"];
+
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    open = false; # Bruuh
+
+    prime = {
+      amdgpuBusId = "PCI:7:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
   # User config
   users.groups.gaspard = {
@@ -35,37 +74,6 @@
     extraSpecialArgs = {inherit inputs;};
     users = {
       "gaspard" = import ../../users/gaspard.nix;
-    };
-  };
-
-  # Programs
-  environment.systemPackages = [
-    pkgs.git
-    pkgs.ncdu
-    pkgs.neofetch
-    pkgs.bottom
-    pkgs.htop
-  ];
-
-  # Services
-  services.seatd.enable = true;
-  services.power-profiles-daemon.enable = true;
-
-  # NVIDIA
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["nvidia-x11" "nvidia-settings"];
-
-  hardware.graphics.enable = true;
-
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-
-    open = false; # Bruuh
-
-    prime = {
-      amdgpuBusId = "PCI:7:0:0";
-      nvidiaBusId = "PCI:1:0:0";
     };
   };
 
