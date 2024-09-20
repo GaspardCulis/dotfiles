9fe9cc09c596027387d26ebb2b75b75ef0849e04
GaspardCulis <gaspard@gasdev.fr>
2024-09-20 12:56:27 +0000

    Zephyrus: Fix hardware-config missing grub device

--- hosts/Zephyrus/hardware-configuration.nix
+++ hosts/Zephyrus/hardware-configuration.nix
@@ -4,7 +4,6 @@
 {
   config,
   lib,
-  pkgs,
   modulesPath,
   ...
 }: {
@@ -13,10 +12,22 @@
     ./disko-config.nix
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "sdhci_pci"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
+  boot = {
+    kernelModules = ["kvm-amd"];
+    extraModulePackages = [];
+    initrd = {
+      availableKernelModules = ["nvme" "xhci_pci" "usbhid" "sdhci_pci"];
+      kernelModules = [];
+    };
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+      };
+    };
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
