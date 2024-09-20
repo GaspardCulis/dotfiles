2949b77dd0511ec492d1ed6a20d3997e445d53ed
GaspardCulis <gaspard@gasdev.fr>
2024-09-20 17:56:03 +0200

    fix(helix): Fixed "jaaj" theme not being linked by HM

--- editor/helix/default.nix
+++ editor/helix/default.nix
@@ -1,14 +1,12 @@
-{
-  pkgs,
-  ...
-} : {
-  home.packages =[
+{pkgs, ...}: {
+  home.packages = [
     pkgs.helix
   ];
 
   home.file = {
     ".config/helix/config.toml".source = ./config.toml;
     ".config/helix/languages.toml".source = ./languages.toml;
+    ".config/helix/themes/jaaj.toml".source = ./themes/jaaj.toml;
   };
 
   home.sessionVariables = {
