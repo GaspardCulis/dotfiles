027b971f8aff0b733705d23921032ec62e8e4844
GaspardCulis <gaspard@gasdev.fr>
2024-09-20 18:15:00 +0200

    feat(eww -> nix): Link `eww-bard` script to local bin with HM

--- bar/eww/default.nix
+++ bar/eww/default.nix
@@ -1,15 +1,14 @@
-{
-  pkgs,
-  ...
-} : {
+{pkgs, ...}: {
   home.file = {
     ".config/eww".source = ../eww;
+    # Eww-barD script
+    ".local/bin/eww-bard" = {
+      source = ../../bin/eww-bard;
+      executable = true;
+    };
   };
-  
+
   home.packages = [
     pkgs.eww
   ];
 }
-
-
-
