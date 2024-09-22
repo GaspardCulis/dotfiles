f1b7186ca0aaeb80950345aee4d54e56ed016edc
GaspardCulis <gaspard@gasdev.fr>
2024-09-22 09:41:24 +0200

    fix(Zephyrus): Now using pipewire instead of pulseaudio

--- hosts/Zephyrus/default.nix
+++ hosts/Zephyrus/default.nix
@@ -24,12 +24,19 @@
   hardware.bluetooth.powerOnBoot = true;
 
   # Audio
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
   # Services
   services.seatd.enable = true;
   services.blueman.enable = true;
-  services.pipewire.pulse.enable = true;
   services.power-profiles-daemon.enable = true;
 
   # Programs
