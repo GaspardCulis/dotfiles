b8e03ddcdbcc3faca051b558d06dbd425dee9341
Culis Gaspard <culisg@im2ag-211-06.u-ga.fr>
2024-09-23 14:43:54 +0200

    feat(shell -> bash): Added `zoxide` nix package dependency

--- shell/bash/default.nix
+++ shell/bash/default.nix
@@ -1,19 +1,16 @@
-{
-  pkgs,
-  ...
-} : {
+{pkgs, ...}: {
   programs.bash = {
     enable = true;
     bashrcExtra = ". ${./.bashrc}";
   };
-  
+
   home.file = {
     ".bash_aliases".source = ./.bash_aliases;
     ".bash_exec".source = ./.bash_exec;
   };
-  
+
   home.packages = [
     pkgs.starship
+    pkgs.zoxide
   ];
 }
-
