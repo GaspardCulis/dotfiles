6357f999e7dceb2aa84b72bc6f5ec74a76b755bd
GaspardCulis <gaspard@gasdev.fr>
2024-09-23 15:19:39 +0200

    fix(hypr): Cursor theme now defined with `home.pointerCursor`

--- de/hypr/default.nix
+++ de/hypr/default.nix
@@ -46,9 +46,15 @@
     pkgs.firefox
     pkgs.kitty
     pkgs.yazi
-    # Theme
-    pkgs.bibata-cursors
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
 
   wayland.windowManager.hyprland = {
     enable = true;
