d50993c00f0136e4fee657d3f967f0de637bd2f9
GaspardCulis <gaspard@gasdev.fr>
2024-10-01 08:44:51 +0200

    chore(flake): Added `anyrun` and `anixrun` inputs

--- flake.nix
+++ flake.nix
@@ -26,6 +26,15 @@
       url = "github:Dr-42/end-rs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    anyrun = {
+      url = "github:anyrun-org/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    anixrun = {
+      url = "github:GaspardCulis/anixrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
