0a9a7d0d02fb2eab840866c19ee07f777df1b993
GaspardCulis <gaspard@gasdev.fr>
2024-10-11 20:01:22 +0200

    fix(garage): Fixed rpc secret path

---
+++ services/authelia/default.nix
@@ -0,0 +1,30 @@
+{...}: {
+  sops.secrets."authelia/JWT_SECRET".owner = "root";
+  sops.secrets."authelia/SESSION_SECRET".owner = "root";
+  sops.secrets."authelia/STORAGE_PASSWORD".owner = "root";
+  sops.secrets."authelia/STORAGE_ENCRYPTION_KEY".owner = "root";
+
+  services.caddy.virtualHosts."auth.gasdev.fr".extraConfig = ''
+    reverse_proxy http://127.0.0.1:9091
+  '';
+
+  virtualisation.oci-containers.containers = {
+    authelia = {
+      image = "docker.io/authelia/authelia:latest";
+      autoStart = true;
+      ports = ["127.0.0.1:9091:9091"];
+      environment = {
+        AUTHELIA_IDENTITY_VALIDATION_RESET_PASSWORD_JWT_SECRET_FILE = "/secrets/JWT_SECRET";
+        AUTHELIA_SESSION_SECRET_FILE = "/secrets/SESSION_SECRET";
+        AUTHELIA_STORAGE_POSTGRES_PASSWORD_FILE = "/secrets/STORAGE_PASSWORD";
+        AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE = "/secrets/STORAGE_ENCRYPTION_KEY";
+      };
+      volumes = [
+        "/run/secrets/authelia:/secrets"
+        "/etc/authelia/configuration.yml:/config/configuration.yml"
+      ];
+    };
+  };
+
+  environment.etc."authelia/configuration.yml".text = builtins.readFile ./configuration.yml;
+}
---
+++ services/i2p/default.nix
@@ -0,0 +1,27 @@
+{...}: {
+  services.caddy.virtualHosts."console.i2p.gasdev.fr".extraConfig = ''
+    reverse_proxy http://127.0.0.1:7657
+  '';
+
+  services.caddy.virtualHosts."proxy.i2p.gasdev.fr".extraConfig = ''
+    reverse_proxy http://127.0.0.1:7657
+  '';
+
+  virtualisation.oci-containers.containers = {
+    uptime-kuma = {
+      image = "docker.io/geti2p/i2p";
+      autoStart = true;
+      environment = {
+        JVM_XMX = "256m";
+      };
+      ports = [
+        "4444:4444"
+        "6668:6668"
+        "7657:7657"
+        "54321:12345"
+        "54321:12345/udp"
+      ];
+      volumes = ["i2phome:/i2p/.i2p" "i2ptorrents:/i2psnark"];
+    };
+  };
+}
