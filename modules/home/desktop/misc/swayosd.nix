c2381df435a55e8c20ca80a9d5ac9a084dcac984
GaspardCulis <gaspard@gasdev.fr>
2025-03-18 11:10:07 +0100

    feat(desktop): Create swayosd module

---
+++ modules/home/desktop/misc/swayosd.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.gasdev.desktop.swayosd;
+in {
+  options.gasdev.desktop.swayosd = {
+    enable = mkEnableOption "Enable opiniated SwayOSD service";
+  };
+
+  config = mkIf cfg.enable {
+    services.swayosd.enable = true;
+
+    home.file = {
+      ".config/swayosd/style.css".text = ''
+        window {
+          background: #20d7d1;
+          opacity: 0.8;
+        }
+      '';
+    };
+  };
+}
