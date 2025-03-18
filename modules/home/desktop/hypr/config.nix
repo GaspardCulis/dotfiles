bf6ab3c93da2cd92012e3cf5e08a7c1e913948d4
GaspardCulis <gaspard@gasdev.fr>
2025-03-18 08:53:44 +0100

    fix(modules/hypr): Fix env config nix syntax

--- modules/home/desktop/hypr/config.nix
+++ modules/home/desktop/hypr/config.nix
@@ -1,27 +1,26 @@
 {config}: {
   monitor = ",preferred,auto,1";
 
-  env = {
-    GDK_BACKEND = "wayland,x11";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    _JAVA_AWT_WM_NONREPARENTING = "1";
+  env = [
+    "GDK_BACKEND, wayland,x11"
+    "SDL_VIDEODRIVER, wayland"
+    "CLUTTER_BACKEND, wayland"
+    "_JAVA_AWT_WM_NONREPARENTING, 1"
 
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
+    "XDG_CURRENT_DESKTOP, Hyprland"
+    "XDG_SESSION_TYPE, wayland"
+    "XDG_SESSION_DESKTOP, Hyprland"
 
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_QPA_PLATFORM = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_QPA_PLATFORMTHEME = "qt6ct";
+    "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
+    "QT_QPA_PLATFORM, 1"
+    "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
+    "QT_QPA_PLATFORMTHEME, qt6ct"
 
-    XCURSOR_SIZE = 24;
+    "XCURSOR_SIZE, 2"
+    "MOZ_ENABLE_WAYLAND, 1"
 
-    MOZ_ENABLE_WAYLAND = "1";
-
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
+    "WLR_NO_HARDWARE_CURSORS, 1"
+  ];
 
   "$mainMod" = "SUPER";
   bind =
