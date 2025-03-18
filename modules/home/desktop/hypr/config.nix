342db1744d913d8279a9a900690b98de13361f25
GaspardCulis <gaspard@gasdev.fr>
2025-03-18 08:57:21 +0100

    fix(modules/hypr): Fix `col.active_border` syntax

--- modules/home/desktop/hypr/config.nix
+++ modules/home/desktop/hypr/config.nix
@@ -163,8 +163,8 @@
     gaps_in = 5;
     gaps_out = "16, 8, 8, 8";
     border_size = 2;
-    col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-    col.inactive_border = "rgba(595959aa)";
+    "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+    "col.inactive_border" = "rgba(595959aa)";
 
     layout = "hy3";
   };
