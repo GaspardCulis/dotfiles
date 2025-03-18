a5a0be3ecb0a283fc4b5341b25f5a2b67b20c011
GaspardCulis <gaspard@gasdev.fr>
2025-03-18 08:35:26 +0100

    refactor(modules): Move bash config to `home/shell/bash` module

---
+++ modules/home/shell/bash.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.gasdev.shell.bash;
+in {
+  options.gasdev.shell.bash = {
+    enable = mkEnableOption "Enable opiniated bash config";
+    jaaj = {
+      enable = mkOption {
+        description = "Enable jaaj-rs integration";
+        type = types.bool;
+        default = true;
+      };
+      colors = mkOption {
+        description = "Enable package lolcat feature";
+        type = types.bool;
+        default = true;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.bash = {
+      enable = true;
+      historyFileSize = 4294967295;
+      historySize = 4294967295;
+      sessionVariables = {
+        EDITOR = "hx";
+      };
+      initExtra = mkIf cfg.jaaj.enable ''
+        eval "$(starship init bash)"
+        eval "$(zoxide init --cmd cd bash)"
+
+        ${if cfg.jaaj.colors then inputs.jaaj-rs.packages.${pkgs.system}.lolcat else inputs.jaaj-rs.packages.${pkgs.system}.default}/bin/jaaj-rs
+      '';
+      bashrcExtra = ''
+        [[ -z "$FUNCNEST" ]] && export FUNCNEST=100
+        [[ -z "$XDG_CONFIG_HOME" ]] && export XDG_CONFIG_HOME="$HOME/.config"
+
+        bind '"\e[A":history-search-backward'
+        bind '"\e[B":history-search-forward'
+      '';
+      shellAliases = {
+        gs="git status";
+        ga="git add -p";
+        gc="git commit";
+        gp="git push";
+        gpl="git pull";
+
+        ns="nix-shell";
+        nsp="nix-shell -p";
+
+        ls="lsd";
+        l="lsd -alh";
+
+        ip="ip --color=auto";
+        tld="tree -L 2";
+      };
+    };
+    home.packages = [
+      pkgs.starship
+      pkgs.zoxide
+      pkgs.tree
+      pkgs.lsd
+    ];
+  };
+}
