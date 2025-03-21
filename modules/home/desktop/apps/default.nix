ed5da815cf1e2224c7583315ed9f2db4d9c0978a
GaspardCulis <gaspard@gasdev.fr>
2025-03-21 11:26:59 +0100

    feat(desktop): Add default apps

--- modules/home/desktop/apps/default.nix
+++ modules/home/desktop/apps/default.nix
@@ -1,6 +1,35 @@
-{...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.gasdev.desktop.apps;
+in {
   imports = [
     ./alacritty.nix
     ./firefox.nix
   ];
+
+  options.gasdev.desktop.apps = {
+    packages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = with pkgs; [
+        vlc
+        qbittorrent
+        prismlauncher
+        element-desktop
+        webcord
+      ];
+    };
+    extraPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+    };
+  };
+
+  config = mkIf config.gasdev.desktop.enable {
+    home.packages = cfg.packages + cfg.extraPackages;
+  };
 }
