08d5d380d61ea917d48a1d7202d646e0369eb067
GaspardCulis <gaspard@gasdev.fr>
2025-03-21 09:48:44 +0100

    feat(eww): Add widget specific services

--- modules/home/desktop/eww/default.nix
+++ modules/home/desktop/eww/default.nix
@@ -10,6 +10,11 @@ in {
   options.gasdev.desktop.eww = {
     enable = mkEnableOption "Enable opiniated eww service config";
     package = mkPackageOption pkgs "eww" {};
+    widget = {
+      bar.enable = mkEnableOption "Enable widget auto-start service";
+      music.enable = mkEnableOption "Enable widget auto-start service";
+      timer.enable = mkEnableOption "Enable widget auto-start service";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -32,22 +37,57 @@ in {
     ];
 
     systemd.user = {
-      services.eww = {
-        Unit = {
-          Description = "ElKowars wacky widgets";
-          PartOf = ["graphical-session.target"];
+      services = let
+        mkWidget = {
+          name,
+          type ? "simple",
+          monitor ? 0,
+        }: {
+          Unit = {
+            Description = "Eww ${name} widget";
+            PartOf = ["graphical-session.target"];
+            After = ["eww.service"];
+          };
+
+          Service = {
+            Type = "one-shot";
+            ExecStart =
+              if type == "simple"
+              then "${cfg.package}/bin/eww open ${name}"
+              else "${cfg.package}/bin/eww open ${name}:${name}0 --arg ${name}0:monitor=${toString monitor}";
+          };
+
+          Install = {
+            WantedBy = ["graphical-session.target"];
+          };
+        };
+      in {
+        eww = {
+          Unit = {
+            Description = "ElKowars wacky widgets";
+            PartOf = ["graphical-session.target"];
+          };
+
+          Service = {
+            Type = "simple";
+            ExecStart = "${cfg.package}/bin/eww daemon --no-daemonize";
+            Restart = "always";
+            RestartSec = "2s";
+          };
+
+          Install = {
+            WantedBy = ["graphical-session.target"];
+          };
         };
 
-        Service = {
-          Type = "simple";
-          ExecStart = "${cfg.package}/bin/eww daemon --no-daemonize";
-          Restart = "always";
-          RestartSec = "2s";
-        };
+        eww-bar = mkIf cfg.widget.bar.enable (mkWidget {
+          name = "bar";
+          type = "window";
+          monitor = 0;
+        });
 
-        Install = {
-          WantedBy = ["graphical-session.target"];
-        };
+        eww-music = mkIf cfg.widget.music.enable (mkWidget {name = "music";});
+        eww-timer = mkIf cfg.widget.timer.enable (mkWidget {name = "timer";});
       };
     };
   };
