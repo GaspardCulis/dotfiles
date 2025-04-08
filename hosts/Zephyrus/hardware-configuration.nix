e254c1d79c4709af7abcd287986e8c2bd32f2ba2
GaspardCulis <gaspard@gasdev.fr>
2025-04-08 10:32:06 +0200

    feat(Zephyrus): Add nvtop pkg

--- hosts/Zephyrus/hardware-configuration.nix
+++ hosts/Zephyrus/hardware-configuration.nix
@@ -63,14 +63,36 @@
   };
 
   #NVIDIA
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.nvidia
+    nvtopPackages.amd
+  ];
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "nvidia-x11"
       "nvidia-settings"
+      "cuda_sanitizer_api"
+      "cuda_profiler_api"
+      "cuda_cuobjdump"
+      "cuda_cuxxfilt"
+      "cuda_nvdisasm"
+      "cuda_nvml_dev"
+      "cuda_nvprune"
       "cuda_cudart"
-      "libcublas"
+      "cuda-merged"
+      "cuda_cupti"
+      "cuda_nvrtc"
       "cuda_cccl"
       "cuda_nvcc"
+      "cuda_nvtx"
+      "cuda_gdb"
+      "libnpp"
+      "libcufft"
+      "libcublas"
+      "libcurand"
+      "libcusolver"
+      "libcusparse"
+      "libnvjitlink"
       # Steam
       "xow_dongle-firmware"
       "steam"
