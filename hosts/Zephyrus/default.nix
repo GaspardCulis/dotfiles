083f2997bc1b3294595d63ce4122e7415be938f9
GaspardCulis <gaspard@gasdev.fr>
2025-05-19 08:48:27 +0200

    chore(Zephyrus): Define stylix polarity and fonts

--- hosts/Zephyrus/default.nix
+++ hosts/Zephyrus/default.nix
@@ -60,7 +60,29 @@
 
   stylix = {
     enable = true;
-    image = ../../assets/wallpaper.png;
+    image = ../../assets/wallpaper-exyl.png;
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.fira-code;
+        name = "FiraCode Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 
   programs = {
