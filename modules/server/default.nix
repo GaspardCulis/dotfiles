a6812c11890d9f9503e6f0088cfb705022292ad5
GaspardCulis <gaspard@gasdev.fr>
2025-05-20 15:54:44 +0200

    perf(server): Enable auto-prune for podman

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -23,6 +23,7 @@
     oci-containers.backend = "podman";
     podman = {
       enable = true;
+      autoPrune.enable = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
