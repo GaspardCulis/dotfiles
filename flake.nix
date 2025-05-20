7dfcd62dadb629478835cdd4bc59616d2e864b4e
GaspardCulis <gaspard@gasdev.fr>
2025-05-20 15:16:41 +0200

    chore(flake): Update to nixpkgs channel 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Configuration Home Manager jaajesque";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
