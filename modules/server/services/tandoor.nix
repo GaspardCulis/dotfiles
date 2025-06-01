9a05f026fb3190c54d056eb1b33b2a2c5cb4932a
GaspardCulis <gaspard@gasdev.fr>
2025-06-01 19:40:38 +0200

    feat(tandoor): Setup mail

--- modules/server/services/tandoor.nix
+++ modules/server/services/tandoor.nix
@@ -7,6 +7,7 @@
 }:
 with lib; let
   cfg = config.gasdev.services.tandoor;
+  mail = config.gasdev.services.mail;
 in {
   options.gasdev.services.tandoor = {
     enable = mkEnableOption "Enable service";
@@ -28,16 +29,26 @@ in {
     sops.secrets."tandoor/S3_SECRET_ACCESS_KEY".owner = "root";
     sops.secrets."tandoor/POSTGRES_USER".owner = "root";
     sops.secrets."tandoor/POSTGRES_PASSWORD".owner = "root";
+    sops.secrets."tandoor/SMTP_USERNAME".owner = "root";
+    sops.secrets."tandoor/SMTP_PASSWORD".owner = "root";
 
     sops.templates."tandoor.env" = {
       content = ''
         SECRET_KEY=${config.sops.placeholder."tandoor/SECRET_KEY"}
+
         S3_REGION_NAME=garage
         S3_BUCKET_NAME=tandoor
         S3_ENDPOINT_URL=https://s3.gasdev.fr
         S3_CUSTOM_DOMAIN=tandoor.s3web.gasdev.fr
         S3_ACCESS_KEY=${config.sops.placeholder."tandoor/S3_ACCESS_KEY"}
         S3_SECRET_ACCESS_KEY=${config.sops.placeholder."tandoor/S3_SECRET_ACCESS_KEY"}
+
+        EMAIL_HOST=${mail.smtpDomain}
+        EMAIL_PORT=465
+        EMAIL_USE_SSL=1
+        EMAIL_HOST_USER=${config.sops.placeholder."tandoor/SMTP_USERNAME"}
+        EMAIL_HOST_PASSWORD=${config.sops.placeholder."tandoor/SMTP_PASSWORD"}
+        DEFAULT_FROM_EMAIL=tandoor@gasdev.fr
       '';
       owner = "root";
     };
