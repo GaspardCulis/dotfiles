d7b39485dba801175ee6364487bc9d0c7f5e1ef6
GaspardCulis <gaspard@gasdev.fr>
2025-06-07 00:30:46 +0200

    fix(flake): Pleaaaase

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in rec {
-    nixosModules.default = import ./modules/system {inherit inputs;};
+    nixosModules.default = import ./modules/system inputs;
     nixosModules."server" = import ./modules/server;
 
     homeManagerModules.default = import ./modules/home;
