3b2afa3ff98e4a04a4e2e2b63e9f541df0687e62
GaspardCulis <gaspard@gasdev.fr>
2025-07-30 18:38:45 +0200

    fix(Zephyrus): Disable currently broken SteamOS

--- hosts/Zephyrus/default.nix
+++ hosts/Zephyrus/default.nix
@@ -38,7 +38,7 @@
         "networkmanager"
       ];
     };
-    specialisations.steamos.enable = true;
+    # specialisations.steamos.enable = true;
     specialisations.guest.enable = true;
   };
 
