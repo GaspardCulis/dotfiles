e173df4353faf8fd736fb3ecd4fed4f0ab981198
GaspardCulis <gaspard@gasdev.fr>
2025-08-07 11:31:11 +0200

    perf(Zephyrus): Replace thermald with asusd

--- hosts/Zephyrus/default.nix
+++ hosts/Zephyrus/default.nix
@@ -47,7 +47,7 @@
   services.blueman.enable = true;
   services.udisks2.enable = true;
   services.gvfs.enable = true;
-  services.thermald.enable = true;
+  services.asusd.enable = true;
   services.power-profiles-daemon.enable = true;
   systemd.services.NetworkManager-wait-online.enable = false; # Faster boot time
 
