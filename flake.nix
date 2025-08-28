1dede84eff7042b89e6c7f588e4a5f593dde3fe2
GaspardCulis <gaspard@gasdev.fr>
2025-08-28 21:42:29 +0200

    fix(flake): Don't follow system nixpkgs for jaaj-rs

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
 
     jaaj-rs = {
       url = "git+https://git.ahur.ac/Jaaj-San/jaaj-rs?rev=e5e5596e85e57ec8a0c4aeb34f64c423320c532d";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs"; # Take advantage of gasdev cachix
     };
 
     udiskr = {
