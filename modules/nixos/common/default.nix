07e2dd6e86f63bd344e9451e2f51a1f2dc700b83
GaspardCulis <gaspard@gasdev.fr>
2025-09-29 15:31:33 +0200

    refactor(modules): Organize nixos modules to use with nixos-unified

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,15 +1,17 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   imports = [
     ./podman.nix
     ./openssh.nix
     ./users
-    ./specialisations/steamos.nix
-    ./specialisations/guest.nix
   ];
 
   nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    substituters = ["https://gasdev.cachix.org"];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [ "https://gasdev.cachix.org" ];
   };
 
   time.timeZone = "Europe/Paris";
---
+++ modules/nixos/default.nix
@@ -0,0 +1,8 @@
+# This is your nixos configuration.
+# For home configuration, see /modules/home/*
+{ flake, ... }:
+{
+  imports = [
+    flake.inputs.self.nixosModules.common
+  ];
+}
