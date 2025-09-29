62d64e74dbf94c1c425a61556bab6428a24bfe34
GaspardCulis <gaspard@gasdev.fr>
2025-09-29 22:20:23 +0200

    fix(gaspard): Fix `allowed-unfree.nix` import path

--- configurations/home/gaspard/desktop.nix
+++ configurations/home/gaspard/desktop.nix
@@ -8,7 +8,7 @@
 in {
   imports = [
     ./default.nix
-    ../../utils/allowed-unfree.nix
+    ../../../utils/allowed-unfree.nix
     self.homeModules.desktop
     inputs.niri-flake.homeModules.niri
   ];
