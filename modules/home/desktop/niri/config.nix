3619bf144c4df186a9e48a7136e8d66954134111
GaspardCulis <gaspard@gasdev.fr>
2025-12-16 09:54:40 +0100

    fix(niri): Tweak preset and default column widths

--- modules/home/desktop/niri/config.nix
+++ modules/home/desktop/niri/config.nix
@@ -140,12 +140,13 @@ in {
 
   layout = {
     default-column-width = {
-      proportion = 2. / 3.;
+      proportion = 9. / 10.;
     };
     preset-column-widths = [
       {proportion = 1. / 3.;}
       {proportion = 1. / 2.;}
       {proportion = 2. / 3.;}
+      {proportion = 9. / 10.;}
     ];
   };
 
