014ad948bd0028883142ea02de5cd45229655708
GaspardCulis <gaspard@gasdev.fr>
2026-03-17 16:28:40 +0100

    chore(packages): Update `turn-rs` package to `v4.0.0`

--- packages/turn-rs.nix
+++ packages/turn-rs.nix
@@ -1,12 +1,15 @@
 {pkgs, ...}:
 pkgs.rustPlatform.buildRustPackage {
   pname = "turn-rs";
-  version = "3.4.0";
-  src = builtins.fetchGit {
+  version = "4.0.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "mycrl";
+    repo = "turn-rs";
     url = "https://github.com/mycrl/turn-rs";
-    rev = "c292a8e4f255a893a75b06977eaaa38c58cabc6f";
+    rev = "v4.0.0";
+    hash = "sha256-HYrFnwxj4BRk2PGK52il5Bh5hxvWHJNQ/5Y0u5FSTwY=";
   };
-  cargoHash = "sha256-wnbovuxh3wc1TU8BYZEOG/8SO9bCUd0eWRC81MtAdqo=";
+  cargoHash = "sha256-q1kq2ISzKZfJfKTRkMrVMQqngAqG2INfej9lGoll5c0=";
 
   nativeBuildInputs = [
     pkgs.pkg-config
