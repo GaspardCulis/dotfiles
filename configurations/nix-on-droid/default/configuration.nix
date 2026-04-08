1f5d85368035c37f3d65053ac543a62bca796030
GaspardCulis <gaspard@gasdev.fr>
2026-04-08 17:52:46 +0200

    fix(nix-on-droid): Fix nix option names

--- configurations/nix-on-droid/default/configuration.nix
+++ configurations/nix-on-droid/default/configuration.nix
@@ -6,14 +6,12 @@
   inherit (flake) inputs;
   inherit (inputs) self;
 in {
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
     substituters = ["https://gasdev.cachix.org"];
-    trusted-substituters = ["https://gasdev.cachix.org"];
-    trusted-public-keys = ["gasdev.cachix.org-1:eBesrrBJpsMZ33OmvG4aKvfdyVkDa2OKCJ2o80IMJfE="];
+    trustedPublicKeys = ["gasdev.cachix.org-1:eBesrrBJpsMZ33OmvG4aKvfdyVkDa2OKCJ2o80IMJfE="];
   };
 
   # Simply install just the packages
