3c7f0e016bfb4ff99aeacdb298f7dd9012880dfb
GaspardCulis <gaspard@gasdev.fr>
2026-04-11 11:11:10 +0200

    feat(nix-on-droid): Add termux tools

--- configurations/nix-on-droid/default/configuration.nix
+++ configurations/nix-on-droid/default/configuration.nix
@@ -18,6 +18,16 @@ in {
     ];
   };
 
+  android-integration = {
+    termux-open-url.enable = true;
+    termux-open.enable = true;
+    termux-setup-storage.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+
+    unsupported.enable = true;
+  };
+
   # Simply install just the packages
   environment.packages = with pkgs; [
     git
