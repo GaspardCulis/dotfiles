abe459c57439c0fdec9454ed737cb67eee33bb72
GaspardCulis <gaspard@gasdev.fr>
2026-04-16 09:17:53 +0200

    chore(probe-rs): Update dl hash

--- configurations/nixos/Zephyrus/probe-rs.nix
+++ configurations/nixos/Zephyrus/probe-rs.nix
@@ -1,7 +1,7 @@
 {...}: let
   probe-rs-rules = builtins.fetchurl {
     url = "https://probe.rs/files/69-probe-rs.rules";
-    sha256 = "1q7ryhl27lalbzzaifxaf0gb18xfzwvpk34sg8l2s1xinb4rr9fi";
+    sha256 = "10lykgbhv5bpir956mdp2sp60rzykmqga0sq9wd8594shglgkw96";
   };
 in {
   users.groups.plugdev.name = "plugdev";
