b83e837d23f3df0583d1cbf5368a603eea9b3d53
GaspardCulis <gaspard@gasdev.fr>
2026-06-08 09:10:19 +0200

    chore(flake): Update to stable release `26.05`

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     # Common
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
